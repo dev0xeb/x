7f469c9880475edec4708f3161ced6525dfeffac
dev0xeb <clintonayomide96@gmail.com>
2024-09-23 22:34:53 +0100

    commit

--- test/OnlineShoopingCart/ShoppingCart.java
+++ test/OnlineShoopingCart/ShoppingCart.java
@@ -1,6 +1,29 @@
 package OnlineShoopingCart;
 
+import java.util.ArrayList;
+
 public class ShoppingCart {
+    private final ArrayList<CartItem>Items;
 
-
+    public ShoppingCart() {
+        this.Items = new ArrayList<>();
+    }
+    public void addItem(Product product, int quantity) {
+        for (CartItem item : Items) {
+            if (item.getProduct().getSku().equals(product.getSku())) {
+                item.setQuantity(item.getQuantity() + quantity);
+                return;
+            }
+        }
+        Items.add(new CartItem(quantity, product));
+    }
+    public void removeItem(Product sku) {
+        Items.removeIf(item -> item.getProduct().getSku().equals(sku.getSku()));
+    }
+    public float getTotalPrice() {
+        return Items.stream().mapToInt(CartItem::getQuantity).sum();
+    }
+    public ArrayList<CartItem> getItems() {
+        return Items;
+    }
 }
