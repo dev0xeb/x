b8995ba8ba249cf17e77653e30d0c9da7120c477
dev0xeb <clintonayomide96@gmail.com>
2024-11-07 11:10:30 +0100

    commit

--- src/dairy/data/models/Diary.java
+++ src/dairy/data/models/Diary.java
@@ -7,7 +7,7 @@ public class Diary {
     private String username;
     private String password;
     private boolean isLocked;
-    private List<Entry> entries = new ArrayList<>();
+//    private List<Entry> entries = new ArrayList<>();
 
     public Diary(){
 
